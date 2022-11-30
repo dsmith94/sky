a83b7cede3ffc66abbe6dba1a06b18d509cbf135
Donald Smith <dsmith94@gmail.com>
2022-11-30 15:01:59 -0800

    Added library description

--- src/game/locations/library.js
+++ src/game/locations/library.js
@@ -2,7 +2,9 @@
 Game.library = () => {
     p$(`
     
-    
+    Golden sunlight filters in from an oculus at the top of a vast dome, which is 
+    almost entirely empty. Crystals of a variety of colors line the walls, and if you listen,
+    you can hear them hum ever so gently.
     
     `)
 }
